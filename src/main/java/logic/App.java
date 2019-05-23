040838eddde7a9071a4cb44b7c5983ed67ff24a6
atuld <atul.duggal@sportsbet.com.au>
2019-05-23 16:39:48 +1000

    me bank

--- src/main/java/logic/App.java
+++ src/main/java/logic/App.java
@@ -1,7 +1,10 @@
 package main.java.logic;
 
 import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
+import java.util.Scanner;
 import java.util.Set;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -14,20 +17,25 @@ public class App {
 
 	public static void main(String[] args) throws IOException {
 		Double balance = null;
-		try {
+//		try {
+		 	String fromTimeStamp="2017-02-03 10:00:30";
+		 	String toTimeStamp="2017-02-04 10:00:30";
+		 	System.out.println(LocalDateTime.parse(fromTimeStamp));
+		 	System.out.println(LocalDateTime.parse(toTimeStamp));
 			BalanceCheck fraud = new BalanceCheck();
-			fraud.setFilePath("records.csv");
-			List<Transaction> result = fraud.mapToModel();
-			if (null != result && !(result.isEmpty()))
-				balance = fraud.BalanceCoreLogic(result);
-			if(balance!=null)
-			{
-				log.info("account balance"+balance);
-
-			}
-		} catch (TransactionException ex) {
-			log.log(Level.SEVERE, "TransactionException in main class", ex);
+//			fraud.setFilePath("records.csv");
+//			fraud.setTimeStampStart(LocalDateTime.parse(start));
+//			fraud.setTimeStampEnd(LocalDateTime.parse(end));
+////			List<Transaction> result = fraud.mapToModel();
+////			if (null != result && !(result.isEmpty()))
+////				balance = fraud.BalanceCoreLogic(result);
+////			if(balance!=null)
+////			{
+////				log.info("account balance"+balance);
+////
+//////			}
+////		} catch (TransactionException ex) {
+//////			log.log(Level.SEVERE, "TransactionException in main class", ex);
+////		}
 		}
-
 	}
-}
